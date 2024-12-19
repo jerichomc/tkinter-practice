34674c383e044bf13ed358ea8e0ebf25f24c35e9
jerichomc <jerichomc255@gmail.com>
2024-12-19 13:01:24 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+window = Tk() #instantiate an instance of a window
+window.geometry("640x640") #height and width of window
+window.title("practice tkinter")
+
+window.mainloop() # will show the window on screen and listen for events
